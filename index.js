712020a649d2b493fa1d0ea7b36a8f8270bf32c4
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-13 22:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n) {
+  const LIS = document.querySelectorAll('.ranked-list');
+ 
+  for (let i = 0; i < LIS.length; i++) {
+   LIS[i].innerHTML = parseInt(LIS[i])+n;
+  }
+}
+function deepestChild() {
+  var str= " .grande-node ";
+  var exitLoop = true;
+  do {
+    if (document
+    .querySelector("str")) {
+      str+= document
+      .querySelector("str") + " ";
+    } else {
+      exitLoop = false;
+    }
+  } while (exitLoop);
+  return document
+   .querySelectorAll("str");
+}
