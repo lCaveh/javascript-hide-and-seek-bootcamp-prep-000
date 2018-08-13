72b8e51056430922de8e704e2b9ddb2415bfff7f
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-13 22:34:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n) {
+  const LIS = document.querySelectorAll('.ranked-list');
+ 
+  for (let i = 0; i < LIS.length; i++) {
+   LIS[i].innerHTML = parseInt(LIS[i])+n;
+  }
+}
+function deepestChild() {
+  let first = document.getElementById('grand-node');
+  let next = first.children[0];
 
+while (next) {
+  first = next;
+  next = first.children[0];
+  }
+  return first;
+  
+}
